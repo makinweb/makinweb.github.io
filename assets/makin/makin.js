35425a26465d708ab05f9e4799fa58e53097716f
Muhammad Ramdan Izzulmakin <makin.ugm@gmail.com>
2020-01-15 06:26:44 +0700

    index page link

--- assets/makin/makin.js
+++ assets/makin/makin.js
@@ -16,6 +16,7 @@ ready(function(){
     //ganti logo
     document.querySelector('.sl-r-banner__brand img').remove();
     document.querySelector('.sl-r-banner__brand a').innerHTML = '<h1>SSSTRAIGHT</h1><p>SCSS/SASS for straight men.</p><h6>disclaimer: this is not initiated by Brendan Eich</h6>'
+    document.querySelector('.sl-r-banner__brand a').setAttribute('href', url_folder+'sass.html');
     
     
 });
